ec3f1e92cc1b1aa2e5821bd8253a2f2c49106fee
mehdiabd <smahdiabdolahi@gmail.com>
2020-03-09 19:56:49 +0330

    recognize.py added.

---
+++ recognize.py
@@ -0,0 +1,42 @@
+__author__ = "Mehdi Abdullahi"
+
+import cv2
+import numpy as n
+from ssl import SSLContext, PROTOCOL_TLSv1
+from urllib.request import urlopen as url
+
+recognize = cv2.cv2.face.LBPHFaceRecognizer_create()
+recognize.read('trainer/trainer.yml')
+cascade = 'haarcascade_frontalface_default.xml'
+faceClassifier = cv2.CascadeClassifier(cascade)
+fontStyle = cv2.FONT_HERSHEY_SIMPLEX
+webcamServerIP = 'https://192.168.1.93:8080/shot.jpg'
+
+while True:
+
+    contxt = SSLContext(PROTOCOL_TLSv1)
+    inf = url(webcamServerIP, context=contxt).read()
+    npImg = n.array(bytearray(inf), dtype=n.uint8)
+    i = cv2.imdecode(npImg, -1)
+    grayscale = cv2.cvtColor(i, cv2.COLOR_BGR2GRAY)
+    face = faceClassifier.detectMultiScale(grayscale, 1.3, 5)
+
+    for x, y, w, z in face:
+
+        cv2.rectangle(i, (x-20, y-20), (x+w+20, y+z+20), (0, 255, 0), 4)
+        ID, person = recognize.predict(grayscale[y:y+z, x:x+w])
+
+        if ID == 1:
+            ID = 'Mehdi'
+        elif ID == 2:
+            ID = 'Abdullah'
+        else:
+            ID = 'Unknown'
+
+        cv2.rectangle(i, (x-22, y-90), (x+w+22, y-22), (0, 255, 0), -1)
+        cv2.putText(i, str(ID), (x, y-40), fontStyle, 2, (255, 255, 255), 3)
+
+    cv2.imshow('im', i)
+
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
