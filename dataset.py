f13083d419766f8b3db6050ce249a453d5008e6c
mehdiabd <smahdiabdolahi@gmail.com>
2020-03-09 08:09:16 +0330

    few edits

--- dataset.py
+++ dataset.py
@@ -1,3 +1,5 @@
+__author__ = "Mehdi Abdullahi"
+
 from urllib.request import urlopen
 from ssl import SSLContext, PROTOCOL_TLSv1
 import numpy as n
@@ -9,6 +11,7 @@ sampleCounter = 0
 ip = 'https://192.168.1.93:8080/shot.jpg'
 
 while True:
+
     contxt = SSLContext(PROTOCOL_TLSv1)
     inf = urlopen(ip, context=contxt).read()
     nImg = n.array(bytearray(inf), dtype=n.uint8)
@@ -17,6 +20,7 @@ while True:
     faces = faceDetect.detectMultiScale(frame2gray, 1.3, 5)
 
     for x, y, w, z in faces:
+
         c.rectangle(imgFrame, (x, y), (x+w, y+z), (255, 0, 0), 2)
         sampleCounter += 1
         c.imwrite('dataset/person.' + str(personId) + '.' + str(sampleCounter) + '.jpg', frame2gray[y:y+z, x:x+w])
